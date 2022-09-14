da9b17dd1429e461dd884218a48deb5abc974b1e
cbarrios <lalobarrios16@yahoo.com>
2022-09-14 11:56:56 -0400

    master: (5) (migration 1 to 2) completed for new added entity field "created"

--- app/src/main/java/com/example/roommigration/room/Database.kt
+++ app/src/main/java/com/example/roommigration/room/Database.kt
@@ -1,5 +1,6 @@
 package com.example.roommigration.room
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.roommigration.room.user.User
@@ -7,7 +8,10 @@ import com.example.roommigration.room.user.UserDao
 
 @Database(
     entities = [User::class],
-    version = 1
+    version = 2,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
 )
 abstract class Database : RoomDatabase() {
 
--- app/src/main/java/com/example/roommigration/room/user/User.kt
+++ app/src/main/java/com/example/roommigration/room/user/User.kt
@@ -1,5 +1,6 @@
 package com.example.roommigration.room.user
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,5 +8,7 @@ import androidx.room.PrimaryKey
 data class User(
     @PrimaryKey(autoGenerate = false)
     val email: String,
-    val username: String
+    val username: String,
+    @ColumnInfo(name = "created", defaultValue = "0")
+    val created: Long = System.currentTimeMillis()
 )
