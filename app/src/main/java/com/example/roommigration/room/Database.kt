752911e0620e8b3a5de3fd57226cac63350385ba
cbarrios <lalobarrios16@yahoo.com>
2022-09-14 12:05:04 -0400

    master: (6) (migration 2 to 3) completed for modified entity field "created" to "createdAt"

--- app/src/main/java/com/example/roommigration/room/Database.kt
+++ app/src/main/java/com/example/roommigration/room/Database.kt
@@ -2,18 +2,25 @@ package com.example.roommigration.room
 
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 import com.example.roommigration.room.user.User
 import com.example.roommigration.room.user.UserDao
+import com.example.roommigration.room.Database as MyDatabase
 
 @Database(
     entities = [User::class],
-    version = 2,
+    version = 3,
     autoMigrations = [
-        AutoMigration(from = 1, to = 2)
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3, spec = MyDatabase.Migration2To3::class)
     ]
 )
 abstract class Database : RoomDatabase() {
 
     abstract val userDao: UserDao
+
+    @RenameColumn(tableName = "User", fromColumnName = "created", toColumnName = "createdAt")
+    class Migration2To3 : AutoMigrationSpec
 }
--- app/src/main/java/com/example/roommigration/room/user/User.kt
+++ app/src/main/java/com/example/roommigration/room/user/User.kt
@@ -10,5 +10,5 @@ data class User(
     val email: String,
     val username: String,
     @ColumnInfo(name = "created", defaultValue = "0")
-    val created: Long = System.currentTimeMillis()
+    val createdAt: Long = System.currentTimeMillis()
 )
